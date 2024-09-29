e2b79ff13e69a9a406bcaaf1993a9f4baadbd0d0
Lawson <liangsi.lu@outlook.com>
2024-09-29 18:43:45 +0800

    update loss

--- 2408_yolo/v1-code/train/YOLO_Loss.py
+++ 2408_yolo/v1-code/train/YOLO_Loss.py
@@ -11,3 +11,14 @@ class YOLO_Loss(nn.Module):
         self.pos_conf = pos_conf
         self.pos_cls = pos_cls
         self.l_noobj = l_noobj
+
+    def iou_force(self, bounding_box, ground_box, gridX, gridY, img_size=448, grid_size=64):
+        # 计算两个box的IoU值
+        # predict_box: [centerX, centerY, width, height]
+        # ground_box : [centerX / self.grid_cell_size - indexJ,centerY / self.grid_cell_size - indexI,(xmax-xmin)/self.img_size,(ymax-ymin)/self.img_size,1,xmin,ymin,xmax,ymax,(xmax-xmin)*(ymax-ymin)
+        # 1.  预处理 predict_box  变为  左上X,Y  右下X,Y  两个边界点的坐标 避免浮点误差 先还原成整数
+        # 不要共用引用
+        # [xmin,ymin,xmax,ymax]
+
+        predict_box = list([0,0,0,0])
+        
